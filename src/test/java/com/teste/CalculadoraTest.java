c0cabbf61565e811ea230b00396208282414b507
rodyokoo <nihonrodrigo@hotmail.com>
2021-08-12 15:03:57 -0300

    mock test

--- src/test/java/com/teste/CalculadoraTest.java
+++ src/test/java/com/teste/CalculadoraTest.java
@@ -3,6 +3,8 @@ package com.teste;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
@@ -13,4 +15,14 @@ public class CalculadoraTest {
 		assertEquals(5, soma);
 	}
 
+	@Test
+	public void testeSomarComMock() {
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		when(calculadora.somar("1+2")).thenReturn(10);
+		
+		int resultado = calculadora.somar("1+2");
+		
+		assertEquals(10, resultado);
+	}
 }
